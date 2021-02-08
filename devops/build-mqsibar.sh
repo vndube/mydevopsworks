e95a9b57e324e02e9a0227587c97da8477a2cbae
Vishwanath Dubey <vishdube@in.ibm.com>
2021-02-08 17:21:46 +0530

    changed build-script

--- devops/build-mqsibar.sh
+++ devops/build-mqsibar.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+mkdir -pv target/BARs
+#cp -r  $builddir/client_impl/$1/src  target/$1
+cp -r  $builddir/client_impl/$clientdir/src  target/$clientdir
 source /opt/ibm/ace-11/server/bin/mqsiprofile
-/opt/ibm/ace-11/server/bin/mqsipackagebar -a $1/devops/packages/package-$1.bar -w . -k target
+#/opt/ibm/ace-11/server/bin/mqsipackagebar -a target/BARs/$1.bar -w ~/workspace/mhdemo/target -k $1
+echo "/opt/ibm/ace-11/server/bin/mqsipackagebar -a target/BARs/$clientdir.bar -w ${WORKSPACE}/target -k $clientdir"
+/opt/ibm/ace-11/server/bin/mqsipackagebar -a target/BARs/$clientdir.bar -w ${WORKSPACE}/target -k $clientdir
+
+mv target/BARs/*  $builddir/devops/packages
+rm -Rf target/*
