b9c32b8b451cdd4b7daa0a89fe5e5a6490d076f2
Vishwanath Dubey <vishdube@in.ibm.com>
2021-02-08 17:51:59 +0530

    added additional dependant files

--- devops/build-mqsibar.sh
+++ devops/build-mqsibar.sh
@@ -2,6 +2,15 @@
 mkdir -pv target/BARs
 #cp -r  $builddir/client_impl/$1/src  target/$1
 cp -r  $builddir/client_impl/$clientdir/src  target/$clientdir
+
+echo "Dependents packages are : $dependents ."
+array=(`echo $dependents | sed 's/,/\n/g'`)
+for dep in "${array[@]}"
+do
+	echo "cp -r $builddir/common/$dep target/$dep"
+        cp -r $builddir/common/$dep target/$dep
+done
+
 source /opt/ibm/ace-11/server/bin/mqsiprofile
 #/opt/ibm/ace-11/server/bin/mqsipackagebar -a target/BARs/$1.bar -w ~/workspace/mhdemo/target -k $1
 echo "/opt/ibm/ace-11/server/bin/mqsipackagebar -a target/BARs/$clientdir.bar -w ${WORKSPACE}/target -k $clientdir"
@@ -9,3 +18,4 @@ echo "/opt/ibm/ace-11/server/bin/mqsipackagebar -a target/BARs/$clientdir.bar -w
 
 mv target/BARs/*  $builddir/devops/packages
 rm -Rf target/*
+
